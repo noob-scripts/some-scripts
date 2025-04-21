fffb5f26594f4a0a67fdcaeabc520f89842377f5
Wide-Scripts <grabriellaemaria@gmail.com>
2025-04-20 22:03:57 -0300

    Create Scythe of the Witch Doctor.lua

---
+++ Wild Hub/Brookhaven/FE Tools/Scythe of the Witch Doctor.lua
@@ -0,0 +1,26 @@
+local lp = game:GetService("Players").LocalPlayer
+local bp = lp.Backpack
+
+local tool = Instance.new("Tool", bp)
+tool.Name = "Plunger tool"
+tool.ToolTip = "Equip a Plunger"
+
+local p = Instance.new("Part", bp)
+p.CanCollide = false
+p.Name = "Handle"
+
+tool.Equipped:Connect(function()
+    local args = {
+        [1] = 8716157625
+    }
+
+    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Wear"):InvokeServer(unpack(args))
+end)
+
+tool.Unequipped:Connect(function()
+    local args = {
+        [1] = 8716157625
+    }
+
+    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Wear"):InvokeServer(unpack(args))
+end)
