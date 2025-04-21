5cd57b5b8f768f79ffcb1697498016c547543201
Wide-Scripts <grabriellaemaria@gmail.com>
2025-04-21 19:12:39 -0300

    Update Plunger.lua

--- Wild Hub/Brookhaven/FE Tools/Plunger.lua
+++ Wild Hub/Brookhaven/FE Tools/Plunger.lua
@@ -26,18 +26,12 @@ local p = Instance.new("Part", bp)
 p.CanCollide = false
 p.Name = "Handle"
 
-tool.Equipped:Connect(function()
+tool:GetPropertyChangedSignal("Parent"):Connect(function()
+    if tool.Parent == bp or tool.Parent == lp.Character then
     local args = {
         [1] = 14062209051
     }
 
     game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Wear"):InvokeServer(unpack(args))
-end)
-
-tool.Unequipped:Connect(function()
-    local args = {
-        [1] = 1406220905114062209051
-    }
-
-    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Wear"):InvokeServer(unpack(args))
+        end
 end)
