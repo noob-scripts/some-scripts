3d12eb6299e2e79807676838c3675b426fdc7931
Wide-Scripts <grabriellaemaria@gmail.com>
2025-04-21 22:07:50 -0300

    Update Library.lua

--- Wild Hub/Library.lua
+++ Wild Hub/Library.lua
@@ -17,35 +17,16 @@ end
 ScreenGui.Parent = gethui() or game.CoreGui
 ScreenGui.ResetOnSpawn = false
 
-local Toggle = Instance.new("TextButton")
+local Toggle = Instance.new("ImageButton")
 Toggle.Name = "Toggle"
 Toggle.Parent = ScreenGui
 Toggle.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Toggle.BackgroundTransparency = 0
-Toggle.TextSize = 25
-Toggle.Text = "+"
+Toggle.BackgroundTransparency = 1
 Toggle.Position = UDim2.new(0, 0, 0.454706937, 0)
 Toggle.Size = UDim2.new(0, 55, 0, 55)
-Toggle.TextColor3 = Color3.fromRGB(255, 255, 255)
+Toggle.ImageId = "rbxassetid://81086834143424"
 Toggle.Draggable = true
 Toggle.Selectable = true
 Toggle.Active = true
-
-local Corner = Instance.new("UICorner")
-Corner.CornerRadius = UDim.new(0.2, 0)
-Corner.Parent = Toggle
-
-local Corner2 = Instance.new("UICorner")
-Corner2.CornerRadius = UDim.new(0.2, 0)
-Corner2.Parent = Image
-
-Toggle.MouseButton1Click:Connect(function()
-    OrionLib:ToggleUi()
-    if Toggle.Text == "+" then
-        Toggle.Text = "-"
-    else
-        Toggle.Text = "+"
-    end
-end)
 
 return OrionLib
