aafeab4fb108f14e5f1e3412b4f5fbbad29b0d38
Wide-Scripts <grabriellaemaria@gmail.com>
2025-04-22 20:27:45 -0300

    Update Library.lua

--- Wild Hub/Library.lua
+++ Wild Hub/Library.lua
@@ -18,18 +18,6 @@ end
 ScreenGui.Parent = gethui() or game.CoreGui
 ScreenGui.ResetOnSpawn = false
 
-function rstr(length)
-	local chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	local result = ""
-	for i = 1, length do
-		local randIndex = math.random(1, #chars)
-		result = result .. chars:sub(randIndex, randIndex)
-	end
-	return result
-end
-
-local name = "Logo-" .. rstr(15) .. "-WHub.png"
-writefile(name, game:HttpGet("https://github.com/noob-scripts/some-scripts/blob/master/Wild%20Hub/WildHub.png"))
 local Toggle = Instance.new("ImageButton")
 Toggle.Name = "Toggle"
 Toggle.Parent = ScreenGui
@@ -37,7 +25,7 @@ Toggle.BackgroundColor3 = Color3.fromRGB(10, 10, 50)
 Toggle.BackgroundTransparency = 0.2
 Toggle.Position = UDim2.new(0, 0, 0.454706937, 0)
 Toggle.Size = UDim2.new(0, 55, 0, 55)
-Toggle.Image = getcustomasset(name)
+Toggle.Image = "rbxassetid://85516588040138"
 Toggle.Draggable = true
 Toggle.Selectable = true
 Toggle.Active = true
@@ -45,6 +33,5 @@ Toggle.MouseButton1Click:Connect(function()
         OrionLib:ToggleUi()
     end)
 task.wait(.1)
-delfile(name)
 
 return OrionLib
