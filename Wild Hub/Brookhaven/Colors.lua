b242164f8daca29394550fc1c59a5558c525d6a8
Wide-Scripts <grabriellaemaria@gmail.com>
2025-05-04 12:54:16 -0300

    Create Colors.lua

---
+++ Wild Hub/Brookhaven/Colors.lua
@@ -0,0 +1,17 @@
+return {
+    Color3.fromRGB(255, 0, 0),
+    Color3.fromRGB(0, 0, 255),
+    Color3.fromRGB(0, 255, 0),
+    Color3.fromRGB(255, 255, 0),
+    Color3.fromRGB(0, 0, 0),
+    Color3.fromRGB(255, 255, 255),
+    Color3.fromRGB(255, 165, 0),
+    Color3.fromRGB(128, 0, 128),
+    Color3.fromRGB(0, 255, 255),
+    Color3.fromRGB(255, 192, 203),
+    Color3.fromRGB(165, 42, 42),
+    Color3.fromRGB(128, 128, 128),
+    Color3.fromRGB(0, 128, 128),
+    Color3.fromRGB(75, 0, 130),
+    Color3.fromRGB(173, 216, 230)
+}
