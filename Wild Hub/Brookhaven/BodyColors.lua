18cea156a2aa56183386aa33b25152a998ae1a27
Wide-Scripts <grabriellaemaria@gmail.com>
2025-05-04 13:27:49 -0300

    Create BodyColors.lua

---
+++ Wild Hub/Brookhaven/BodyColors.lua
@@ -0,0 +1,18 @@
+return {
+    "Really black",
+    "Black",
+    "Magenta",
+    "Navy blue",
+    "Really blue",
+    "Toothpaste",
+    "Forest green",
+    "Lime green",
+    "New Yeller",
+    "Deep orange",
+    "CGA brown",
+    "Maroon",
+    "Really red",
+    "Hot pink",
+    "Pink",
+    "Salmon"
+}
