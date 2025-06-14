4bf9d22f7668fbeca3e6d4806798551eff52cd23
Wide-Scripts <grabriellaemaria@gmail.com>
2025-06-14 16:58:05 -0300

    Create loader.lua

---
+++ Wild Hub/loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId==4924922222 then
+    loadstring(game:HttpGet("https://github.com/noob-scripts/some-scripts/blob/master/Wild%20Hub/Brookhaven.lua?raw=true"))()
+elseif game.PlaceId == 3956818381 then
+    loadstring(game:HttpGet("https://github.com/noob-scripts/some-scripts/blob/master/Wild%20Hub/NinjaLegends.lua?raw=true"))()
+end
