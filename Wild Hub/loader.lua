9b180b86d8391b49187cfd19ea905fbbb01686c8
Wide-Scripts <grabriellaemaria@gmail.com>
2025-07-16 00:05:35 -0300

    Update loader.lua

--- Wild Hub/loader.lua
+++ Wild Hub/loader.lua
@@ -1,7 +1,3 @@
-if setclipboard and hookfunction then
-    local sc;sc=hookfunction(setclipboard, function(b) if b == "https://discord.gg/MZXuwPHp9r" then return sc("https://discord.gg/mhzDcapGgA") end return sc(b) end)
-end
-
 if game.PlaceId==4924922222 then
     loadstring(game:HttpGet("https://github.com/noob-scripts/some-scripts/blob/master/Wild%20Hub/Brookhaven.lua?raw=true"))()
 elseif game.PlaceId == 3956818381 then
