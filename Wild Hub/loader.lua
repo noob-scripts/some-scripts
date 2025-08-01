f425f048a6ca622ca93e5f5c39bf483704346724
Wide-Scripts <grabriellaemaria@gmail.com>
2025-08-01 17:32:46 -0300

    Update loader.lua

--- Wild Hub/loader.lua
+++ Wild Hub/loader.lua
@@ -1,5 +1,11 @@
-if game.PlaceId==4924922222 then
-    loadstring(game:HttpGet("https://github.com/noob-scripts/some-scripts/blob/master/Wild%20Hub/Brookhaven.lua?raw=true"))()
-elseif game.PlaceId == 3956818381 then
-    loadstring(game:HttpGet("https://github.com/noob-scripts/some-scripts/blob/master/Wild%20Hub/NinjaLegends.lua?raw=true"))()
+local scripts = {
+    [4924922222] = "https://github.com/noob-scripts/some-scripts/blob/master/Wild%20Hub/Brookhaven.lua?raw=true",
+    [3956818381] = "https://github.com/noob-scripts/some-scripts/blob/master/Wild%20Hub/NinjaLegends.lua?raw=true"
+}
+
+local placeid = game.PlaceId
+if scripts[placeid] then
+    loadstring(game:HttpGet(scripts[placeid]))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/noob-scripts/some-scripts/refs/heads/master/Wild%20Hub/Universal.lua"))()
 end
