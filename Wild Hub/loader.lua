ec153ed8dfdc61d43f9aa12332544966045c9302
Wild-Scripts <grabriellaemaria@gmail.com>
2025-09-03 20:23:37 -0300

    Update loader.lua

--- Wild Hub/loader.lua
+++ Wild Hub/loader.lua
@@ -2,7 +2,8 @@ local scripts = {
     [4924922222] = "https://github.com/noob-scripts/some-scripts/blob/master/Wild%20Hub/Brookhaven.lua?raw=true",
     [3956818381] = "https://github.com/noob-scripts/some-scripts/blob/master/Wild%20Hub/NinjaLegends.lua?raw=true",
     [155382109] = "https://github.com/noob-scripts/some-scripts/blob/master/Wild%20Hub/Small-Scripts/SAKTKIA51.lua?raw=true",
-    [3101667897] = "https://github.com/noob-scripts/some-scripts/blob/master/Wild%20Hub/LegendsOfSpeed.lua?raw=true"
+    [3101667897] = "https://github.com/noob-scripts/some-scripts/blob/master/Wild%20Hub/LegendsOfSpeed.lua?raw=true",
+    [70671905624144] = "https://github.com/noob-scripts/some-scripts/blob/master/Wild%20Hub/Small-Scripts/StealABaddie.lua.txt?raw=true"
 }
 
 local placeid = game.PlaceId
