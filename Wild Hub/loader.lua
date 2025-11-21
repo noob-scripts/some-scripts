d99945d30adadbbf69b113e0b0a39d2071a4620a
Wild-Scripts <grabriellaemaria@gmail.com>
2025-11-21 12:41:57 -0300

    Create loader.lua

---
+++ Wild Hub/loader.lua
@@ -0,0 +1,20 @@
+
+local SG = Instance.new("ScreenGui", game.CoreGui)
+SG.IgnoreGuiInset = true
+SG.DisplayOrder = 99999999
+
+local F = Instance.new("Frame", SG)
+F.Size = UDim2.new(1,0,1,0)
+F.BackgroundColor3 = Color3.new(0,0,0)
+
+local T = Instance.new("TextLabel", F)
+T.AnchorPoint = Vector2.new(0.5,0.5)
+T.Position = UDim2.new(0.5,0,0.5,0)
+T.Size = UDim2.new(0.8,0,0.12,0)
+T.BackgroundTransparency = 1
+T.TextColor3 = Color3.new(1,1,1)
+T.TextScaled = true
+T.Text = "⚠️ PLEASE EXECUTE THE ORIGINAL WILD HUB SCRIPT, NAGI HUB SKIDDED WILD HUB\nTHE ORIGINAL CREATOR IS WILD WIDE"
+
+task.wait(0.2)
+while true do end
